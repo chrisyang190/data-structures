1c05f20b32bdc9427e18691a77d5ba0f3580520d
chrisyang190 <chrisyang190@gmail.com>
2017-02-10 15:07:25 -0800

    Pass All But Last Test for HashTable

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -39,6 +39,7 @@ describe('hashTable', function() {
     var v1 = 'val1';
     var v2 = 'val2';
     var oldHashFunction = window.getIndexBelowMaxForKey;
+     // debugger;
     window.getIndexBelowMaxForKey = function() { return 0; };
     hashTable.insert(v1, v1);
     hashTable.insert(v2, v2);
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,19 +2,43 @@
 
 var HashTable = function() {
   this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+  this._storage = LimitedArray(this._limit); // storage = []
+  this.counter = true;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tempIdx = index + 1;
+ // var storageKey = this._storage[this._storage.get(index/*0*/)];
+  // debugger;
+
+  // if (tempIdx !== index) {
+  //if (this._storage.get(index) !== undefined) {
+    //console.log('hello');
+    //debugger;
+    //return this._storage.set(index + 1, v); // value = v1
+                                  // value = v2   
+  // } else {
+    return this._storage.set(index, v);
+  // } else {
+  //   var tempIdx = Math.floor(Math.random() * this._limit);
+  //   return this._storage.set(tempIdx, v);
+  // }
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // debugger;
+  return this._storage.get(index); // value
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // debugger;
+  // this._storage[this._storage.get(index)] = undefined;
+  this._storage[this._storage.set(index)] = undefined;
+  //return this._storage[this._storage.get(index)];
 };
 
 
