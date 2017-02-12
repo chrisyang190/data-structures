46627ea2fdc6b7fc3bcbf2c1a5118ffae10465db
chrisyang190 <chrisyang190@gmail.com>
2017-02-11 18:18:45 -0800

    Refactored Hashtable insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,41 +1,29 @@
 var BinarySearchTree = function(value) {
-  //var binaryTree = Object.create(binaryPrototype);
   var binaryTree = new BinaryTreeNode(value);
   _.extend(binaryTree, binaryPrototype);
-
-  // var parentNode = binaryTreeNode(value); // {5: {value: 5, parent: null , left: newNode[value], right: null}, 
-  // binaryTree[value] = parentNode;
-  // {5: {value: 5, left: null, right: null}, 
-  // 2: {value: 2, parent: 5 , left: null, right: null} }
-  //binaryTree obj with new node created
   return binaryTree;
 };
 
 
 var binaryPrototype = {};
 
-binaryPrototype.insert = function(value) { // 2
-  // debugger;
+binaryPrototype.insert = function(value) { 
   var newNode = new BinaryTreeNode(value);
   var parent = this;
-
-  var insertTree = function(node, currentParent) {
-
-
+  var insertTree = function(node) {
     if (value < parent.value && parent.left === undefined) {
       parent.left = newNode;
     } else if (value < parent.value && parent.left !== undefined) { //recursive case
       parent = parent.left;
-      insertTree(newNode, parent);
+      insertTree(newNode);
     } else if (value > parent.value && parent.right === undefined) {
       parent.right = newNode;
     } else if (value > parent.value && parent.right !== undefined) {
       parent = parent.right;
-      insertTree(newNode, parent);
+      insertTree(newNode);
     }
   };
-
-  insertTree(newNode, parent);
+  insertTree(newNode);
 };
   
 
@@ -43,7 +31,6 @@ binaryPrototype.insert = function(value) { // 2
 binaryPrototype.contains = function(value) {
   var currentNode = this;
   var containsVal = false;
-  //debugger;
   var traverseTree = function(node) {
     if (value === node.value) {
       containsVal = true;
@@ -61,10 +48,9 @@ binaryPrototype.contains = function(value) {
       } else {
         return;
       }
-    }   
+    }
   };
   traverseTree(currentNode);
-
   return containsVal;
 };
 
@@ -85,38 +71,16 @@ binaryPrototype.depthFirstLog = function (cb) {
       traverseTree(currentNode);
     } else if (currentNode.right === undefined && currentNode.left === undefined) {
       return;
-      //cb(currentNode);
     }
-    // else if (currentNode.left !== undefined) {
-    //   currentNode = currentNode.left;
-    // } 
-    // else if (currentNode.left !== undefined) {
-    //   currentNode = currentNode.left;
-    // } 
-
   };
   traverseTree(currentNode);
-
-// traverse through tree
-
-
-
 };
 
 var BinaryTreeNode = function(value) {
   var node = {};
-
   node.value = value;
-  // node.children = {};
-  // node.children.left = null;
-  // node.children.right = null;
   node.left = undefined;
   node.right = undefined;
-  //node.children = [];
-  //node.next = null;
-  // node.addChild = function(value) {
-  //   this.children.push(treeNode(value));
-  // };
   return node;
 };
 
